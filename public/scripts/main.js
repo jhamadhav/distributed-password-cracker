d3a81d44cc18d875f2e3d613b76a9241c515979d
jhamadhav <jhamadhav28@gmail.com>
2022-07-08 23:44:05 +0530

    styling added to index/landing page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
-
+let linkLen = 12
 let s = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 const rand = (min = 0, max = 1) => {
@@ -30,9 +30,30 @@ document.getElementById("create").onclick = () => {
 document.getElementById("join").onclick = () => {
     let roomID = document.getElementById("roomID").value
 
-    if (roomID.toString().length < 12) {
+    if (roomID.toString().length < linkLen) {
         console.log("Invalid room id")
         return 0;
     }
     window.location = `/room/${roomID}`
+}
+
+// style for input button
+let inpBtn = document.getElementById("roomID")
+inpBtn.oninput = () => {
+    let joinBtn = document.getElementById("join")
+    console.log(inpBtn.value.length);
+
+    joinBtn.style.color = "#0477fd"
+    if (inpBtn.value.length == 0) {
+        joinBtn.style.color = "white"
+
+    }
+    if (inpBtn.value.length >= linkLen) {
+        joinBtn.disabled = false
+        joinBtn.style.background = "#0477fd"
+        joinBtn.style.color = "white"
+    } else {
+        joinBtn.disabled = true
+        joinBtn.style.background = "transparent"
+    }
 }
